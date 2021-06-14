3e55ff107c449e02602feff3159c4e10d95c4772
Dreamedc2015 <66358133+Dreamedc2015@users.noreply.github.com>
2021-06-14 22:32:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,12 @@ const TOKEN = process.env.TOKEN;
 
 const bedrock = require("bedrock-protocol");
 const client = bedrock.createClient({
-  host: "tailvile.xyz", // optional
-  port: 19132, // optional, default 19132
-  username: "Dreamedc2015", // the username you want to join as, optional if online mode
-  offline: false, // optional, default false. if true, do not login with Xbox Live. You will not be asked to sign-in if set to true.
-  // Optional for some servers which verify the title ID:
-  // authTitle: bedrock.title.MinecraftNintendoSwitch
+  host: "tailvile.xyz",
+  username: "Dreamedc2015",
+  offline: false, 
 });
 
 client.on("text", (packet) => {
-  // Listen for chat messages and echo them back.
   if (packet.source_name != client.options.username) {
       tv = bot.guilds.cache.get("830180687759802392");
     tv.channels.cache
